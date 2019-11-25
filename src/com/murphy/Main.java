5b35fd14ca9e5e92d5dd6caf9f335fb2088e907e
Murphy Hsieh <murphyhsieh@leandev.io>
2019-11-25 21:14:34 +0800

    initial commit. using List, ArrayList archieved Theatre class with inner class Seat

---
+++ src/com/murphy/Main.java
@@ -0,0 +1,22 @@
+package com.murphy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Theatre theatre = new Theatre("Taipei", 8, 12);
+//	    theatre.getSeats();
+
+	    if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+    }
+}
