0c595d9439a9dc3fce101275aae4e288132757ef
Murphy Hsieh <murphyhsieh@leandev.io>
2020-05-22 10:47:08 +0800

    Set challenge 作業內容，跟第一步改寫 HeavenlyBody class

--- src/com/murphy/HeavenlyBody.java
+++ src/com/murphy/HeavenlyBody.java
@@ -3,15 +3,26 @@ package com.murphy;
 import java.util.HashSet;
 import java.util.Set;
 
-public final class HeavenlyBody {
+public class HeavenlyBody {
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
+    private final BodyTypes bodyType;
 
-    public HeavenlyBody(String name, double orbitalPeriod) {
+    public enum BodyTypes {
+        STAR,
+        PLANET,
+        DWARF_PLANET,
+        MOON,
+        COMET,
+        ASTEROID
+    }
+
+    public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
         this.name = name;
         this.orbitalPeriod = orbitalPeriod;
         this.satellites = new HashSet<>();
+        this.bodyType = bodyType;
     }
 
     public String getName() {
@@ -22,7 +33,11 @@ public final class HeavenlyBody {
         return orbitalPeriod;
     }
 
-    public boolean addMoon(HeavenlyBody moon) {
+    public BodyTypes getBodyType() {
+        return bodyType;
+    }
+
+    public boolean addSatellites(HeavenlyBody moon) {
         return this.satellites.add(moon);
     }
 
@@ -32,24 +47,29 @@ public final class HeavenlyBody {
 
 
     @Override
-    public boolean equals(Object obj) {
+    public final boolean equals(Object obj) {
         if(this == obj) {
             return true;
         }
 
-//        System.out.println("Obj.getClass() is " + obj.getClass());
-//        System.out.println("this.getClass() is " + this.getClass());
-        if((obj == null) || (obj.getClass() != this.getClass())) {
-            return false;
+        if(obj instanceof HeavenlyBody) {
+            HeavenlyBody theObj= (HeavenlyBody) obj;
+            if(this.name.equals(theObj.getName())) {
+                return this.getBodyType() == theObj.getBodyType();
+            }
         }
-
-        String objName = ((HeavenlyBody) obj).getName();
-        return this.name.equals(objName);
+        return false;
     }
 
     @Override
-    public int hashCode() {
-//        System.out.println("hashcode called: " + this.name.hashCode() + 57);
-        return this.name.hashCode() + 57;
+    public final int hashCode() {
+        /** enum 本身有提供 hashCode() **/
+        return this.name.hashCode() + 57 + this.bodyType.hashCode();
     }
+
+    @Override
+    public String toString() {
+        return this.name + ": " + this.bodyType + ", " + this.orbitalPeriod;
+    }
+
 }
