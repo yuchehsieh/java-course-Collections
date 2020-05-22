05ac4c1ed3903c450f0eabcb33af7119413b5a7d
Murphy Hsieh <murphyhsieh@leandev.io>
2020-05-22 09:24:27 +0800

    Set 的非對稱方法 聯集、差集

--- src/com/murphy/SetMain.java
+++ src/com/murphy/SetMain.java
@@ -12,7 +12,7 @@ public class SetMain {
         Set<Integer> squares = new HashSet<>();
         Set<Integer> cubes = new HashSet<>();
 
-        for(int i=1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             squares.add(i * i);
             cubes.add(i * i * i);
         }
@@ -25,7 +25,7 @@ public class SetMain {
         Set<Integer> intersection = new HashSet<>(squares);
         intersection.retainAll(cubes);
         System.out.println("Intersection contains " + intersection.size() + " elements.");
-        for(int i : intersection) {
+        for (int i : intersection) {
             System.out.println(i + " is the square of " + Math.sqrt(i) + " and the cube of " + Math.cbrt(i));
         }
 
@@ -34,51 +34,55 @@ public class SetMain {
         String[] arrayWords = sentence.split(" ");
         words.addAll(Arrays.asList(arrayWords));
 
-        for(String s : words) {
+        for (String s : words) {
             System.out.println(s);
         }
-
-        System.out.println("========================");
 
         Set<String> nature = new HashSet<>();
         Set<String> divine = new HashSet<>();
         String[] natureWords = {"all", "nature", "is", "but", "art", "unknown", "to", "thee"};
         nature.addAll(Arrays.asList(natureWords));
 
-        for(String s : nature) {
-            System.out.println(s);
-        }
-
         String[] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
         divine.addAll(Arrays.asList(divineWords));
 
-        System.out.println("========================");
+        System.out.println("nature - divine:");
+        Set<String> diff1 = new HashSet<>(nature);
+        diff1.removeAll(divine);
+        printSet(diff1);
 
+        System.out.println("divine - nature:");
+        Set<String> diff2 = new HashSet<>(divine);
+        diff2.removeAll(nature);
+        printSet(diff2);
 
-        for(String s : divine) {
-            System.out.println(s);
+        Set<String> unionTest = new HashSet<>(nature);
+        unionTest.addAll(divine);
+        Set<String> intersectionTest = new HashSet<>(nature);
+        intersectionTest.retainAll(divine);
+
+        System.out.println("Symmetric difference");
+        unionTest.removeAll(intersectionTest);
+        printSet(unionTest);
+
+        if(nature.containsAll(divine)) {
+            System.out.println("divine is a subset of nature");
         }
 
+        if(nature.containsAll(intersectionTest)) {
+            System.out.println("intersection is  subset of nature");
+        }
 
+        if(divine.containsAll(intersectionTest)) {
+            System.out.println("intersection is a subset of divine");
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private static void printSet(Set<String> set) {
+        System.out.print("\t");
+        for(String s : set) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
     }
 }
