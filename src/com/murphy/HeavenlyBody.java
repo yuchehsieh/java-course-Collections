f5b39e57d5a87f798df569bd3ced019b11a9488a
Murphy Hsieh <murphyhsieh@leandev.io>
2020-05-22 11:13:54 +0800

    改寫 Main.java 實例變數產生方式 mark HeavenlyBody class as abstract, 測試案例 1~3

--- src/com/murphy/HeavenlyBody.java
+++ src/com/murphy/HeavenlyBody.java
@@ -3,7 +3,7 @@ package com.murphy;
 import java.util.HashSet;
 import java.util.Set;
 
-public class HeavenlyBody {
+public abstract class HeavenlyBody {
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
---
+++ src/com/murphy/Planet.java
@@ -0,0 +1,17 @@
+package com.murphy;
+
+public class Planet extends HeavenlyBody{
+
+    public Planet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.PLANET);
+    }
+
+    @Override
+    public boolean addSatellites(HeavenlyBody moon) {
+        if(this.getBodyType() == BodyTypes.MOON) {
+            return super.addSatellites(moon);
+        } else {
+            return false;
+        }
+    }
+}
